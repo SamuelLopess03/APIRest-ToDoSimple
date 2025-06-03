886843461a725dff29b86367a42c62c58fa591ec
Samuel Lopes <samuellopes12390@gmail.com>
2025-06-03 11:39:14 -0300

    feat(repositories): Created User and Task

---
+++ src/main/java/com/projectbackend/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,20 @@
+package com.projectbackend.todosimple.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projectbackend.todosimple.models.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+	
+	//@Query(value = "SELECT t FROM Task t WHERE t.user.id = :id")
+	//List<Task> findByUser_Id(@Param("id") Long id);
+	
+	//@Query(value = "SELECT * FROM Task t WHERE t.user_id = :id", nativeQuery = true)
+	//List<Task> findByUser_Id(@Param("id") Long id);
+	
+	List<Task> findByUser_Id(Long id);
+}
