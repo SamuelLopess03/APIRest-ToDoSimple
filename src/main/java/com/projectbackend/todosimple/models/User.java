921ede58c8938c93714b54f06cf81c1eec81c3d8
Samuel Lopes <samuellopes12390@gmail.com>
2025-06-03 11:11:20 -0300

    feat: Create Task Model

--- src/main/java/com/projectbackend/todosimple/models/User.java
+++ src/main/java/com/projectbackend/todosimple/models/User.java
@@ -1,5 +1,7 @@
 package com.projectbackend.todosimple.models;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -10,6 +12,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -41,7 +44,8 @@ public class User {
 	@Size(groups = { CreateUser.class, UpdatedUser.class }, min = 8, max = 60)
 	private String password;
 	
-	//private List<Task> tasks = new ArrayList<Task>();
+	@OneToMany(mappedBy = "user")
+	private List<Task> tasks = new ArrayList<Task>();
 	
 	public User() {}
 
@@ -73,6 +77,14 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Task> getTasks() {
+		return tasks;
+	}
+
+	public void setTasks(List<Task> tasks) {
+		this.tasks = tasks;
 	}
 
 	@Override
