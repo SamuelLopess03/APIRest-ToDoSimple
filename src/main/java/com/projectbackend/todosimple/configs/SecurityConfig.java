84d6c0ca845a6501964f1ffb9dd325a8a9d91b87
Samuel Lopes <samuellopes12390@gmail.com>
2025-06-08 12:15:49 -0300

    feat: Add Dependencies and Create SecurityConfig

---
+++ src/main/java/com/projectbackend/todosimple/configs/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.projectbackend.todosimple.configs;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+	
+	private static final String[] PUBLIC_MATCHERS = {
+		"/"
+	};
+	
+	private static final String[] PUBLIC_MATCHERS_POST = {
+		"/user",
+		"/login"
+	};
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf(AbstractHttpConfigurer::disable)
+	    .authorizeHttpRequests(auth -> auth
+	    		.requestMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+	    		.requestMatchers(PUBLIC_MATCHERS).permitAll()
+	    		.anyRequest().authenticated())
+	    .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		
+		return http.build();
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+	    configuration.setAllowCredentials(true);
+
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
+	    
+	    return source;
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
