f01e4020d3ba5343e4a60a610375fe9f1d8d0fca
Samuel Lopes <samuellopes12390@gmail.com>
2025-06-12 15:59:48 -0300

    feat: Set UserService Authorization

--- src/main/java/com/projectbackend/todosimple/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/projectbackend/todosimple/exceptions/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.validation.FieldError;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.projectbackend.todosimple.services.exceptions.AuthorizationException;
 import com.projectbackend.todosimple.services.exceptions.DataBindingViolationException;
 import com.projectbackend.todosimple.services.exceptions.ObjectNotFoundException;
 
@@ -48,6 +50,42 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler
 		
 		response.getWriter().append(errorResponse.toJson());
 	}
+	
+	@ExceptionHandler(AuthenticationException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseEntity<Object> handleAuthenticationException(
+            AuthenticationException authenticationException,
+            WebRequest request) {
+        log.error("Authentication error ", authenticationException);
+        return buildErrorResponse(
+                authenticationException,
+                HttpStatus.UNAUTHORIZED,
+                request);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ResponseEntity<Object> handleAccessDeniedException(
+            AccessDeniedException accessDeniedException,
+            WebRequest request) {
+        log.error("Authorization error ", accessDeniedException);
+        return buildErrorResponse(
+                accessDeniedException,
+                HttpStatus.FORBIDDEN,
+                request);
+    }
+
+    @ExceptionHandler(AuthorizationException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ResponseEntity<Object> handleAuthorizationException(
+            AuthorizationException authorizationException,
+            WebRequest request) {
+        log.error("Authorization error ", authorizationException);
+        return buildErrorResponse(
+                authorizationException,
+                HttpStatus.FORBIDDEN,
+                request);
+    }
 
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
--- src/main/java/com/projectbackend/todosimple/services/UserService.java
+++ src/main/java/com/projectbackend/todosimple/services/UserService.java
@@ -1,10 +1,12 @@
 package com.projectbackend.todosimple.services;
 
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +14,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.projectbackend.todosimple.models.User;
 import com.projectbackend.todosimple.models.enums.ProfileEnum;
 import com.projectbackend.todosimple.repositories.UserRepository;
+import com.projectbackend.todosimple.security.UserSpringSecurity;
+import com.projectbackend.todosimple.services.exceptions.AuthorizationException;
 import com.projectbackend.todosimple.services.exceptions.DataBindingViolationException;
 import com.projectbackend.todosimple.services.exceptions.ObjectNotFoundException;
 
@@ -26,6 +30,12 @@ public class UserService {
 
 	
 	public User findById(Long id) {
+		UserSpringSecurity userSpringSecurity = authenticated();
+		
+		if(!Objects.nonNull(userSpringSecurity) || !userSpringSecurity.hasRole(ProfileEnum.ADMIN)
+				&& !id.equals(userSpringSecurity.getId()))
+			throw new AuthorizationException("Acesso Negado");
+		
 		Optional<User> user = this.userRepository.findById(id);
 		
 		return user.orElseThrow(() -> new ObjectNotFoundException(
@@ -60,6 +70,15 @@ public class UserService {
 			this.userRepository.deleteById(id);
 		} catch (Exception e) {
 			throw new DataBindingViolationException("Não é Possível Excluir, Pois há Entidades Relacionadas!");
+		}
+	}
+	
+	public static UserSpringSecurity authenticated() {
+		try {
+			return (UserSpringSecurity) SecurityContextHolder.getContext()
+					.getAuthentication().getPrincipal();
+		} catch (Exception e) {
+			return null;
 		}
 	}
 }
