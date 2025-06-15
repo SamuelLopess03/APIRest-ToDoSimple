e0af880875cdcbd8cd619e0694bf90a511679674
Samuel Lopes <samuellopes12390@gmail.com>
2025-06-15 10:31:10 -0300

    chore: Removing Unused Dependencies

--- src/main/java/com/projectbackend/todosimple/repositories/UserRepository.java
+++ src/main/java/com/projectbackend/todosimple/repositories/UserRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.projectbackend.todosimple.models.User;
-import java.util.List;
 
 
 @Repository
--- src/main/java/com/projectbackend/todosimple/services/UserDetailsServiceImpl.java
+++ src/main/java/com/projectbackend/todosimple/services/UserDetailsServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import com.projectbackend.todosimple.controllers.TaskController;
+
 import com.projectbackend.todosimple.models.User;
 import com.projectbackend.todosimple.repositories.UserRepository;
 import com.projectbackend.todosimple.security.UserSpringSecurity;
